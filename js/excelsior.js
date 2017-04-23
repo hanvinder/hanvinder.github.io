23b98c08b49b1c4421a11d2105d5e538226b95af
hanvinder <hanvinder@gmail.com>
2017-04-23 15:43:29 -0400

    first commit

---
+++ js/excelsior.js
@@ -0,0 +1,102 @@
+/*!
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+ $(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('body').on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+
+// Scroll up
+ $(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+    $('.scroll').on('click', function(e){		
+		e.preventDefault()
+    
+  $('html, body').animate({
+      scrollTop : $(this.hash).offset().top
+    }, 1500);
+});
+
+});
+
+
+//animation
+jQuery(function($) {
+  
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function() {
+    
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+        $animatables = $('.animatable');
+    
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.size() == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+    
+    // Check all animatables and animate them if necessary
+		$animatables.each(function(i) {
+       var $animatable = $(this);
+			if (($animatable.offset().top + $animatable.height() - 100) < offset) {
+        $animatable.removeClass('animatable').addClass('animated');
+			}
+    });
+
+	};
+  
+  // Hook doAnimations on scroll, and trigger a scroll
+	$(window).on('scroll', doAnimations);
+  $(window).trigger('scroll');
+
+});
+
---
+++ js/top.js
@@ -0,0 +1,139 @@
+ $(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+    $('.scroll').on('click', function(e){		
+		e.preventDefault()
+    
+  $('html, body').animate({
+      scrollTop : $(this.hash).offset().top
+    }, 1500);
+});
+
+});
+
+
+
+
+// Hide and show when click to in work
+$('#prog').on('click', function () {
+   $('#design').hide();
+   $('#programm').fadeIn();
+});
+
+$('#desi').on('click', function () {
+   $('#programm').hide();
+   $('#design').fadeIn();
+});
+
+
+jQuery(document).ready(function(event){
+  var isAnimating = false,
+    newLocation = '';
+    firstLoad = false;
+  
+  //trigger smooth transition from the actual page to the new one 
+  $('main').on('click', '[data-type="page-transition"]', function(event){
+    event.preventDefault();
+    //detect which page has been selected
+    var newPage = $(this).attr('href');
+    //if the page is not already being animated - trigger animation
+    if( !isAnimating ) changePage(newPage, true);
+    firstLoad = true;
+  });
+
+  //detect the 'popstate' event - e.g. user clicking the back button
+  $(window).on('popstate', function() {
+  	if( firstLoad ) {
+      /*
+      Safari emits a popstate event on page load - check if firstLoad is true before animating
+      if it's false - the page has just been loaded 
+      */
+      var newPageArray = location.pathname.split('/'),
+        //this is the url of the page to be loaded 
+        newPage = newPageArray[newPageArray.length - 1];
+
+      if( !isAnimating  &&  newLocation != newPage ) changePage(newPage, false);
+    }
+    firstLoad = true;
+	});
+
+	function changePage(url, bool) {
+    isAnimating = true;
+    // trigger page animation
+    $('body').addClass('page-is-changing');
+    $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+    	loadNewContent(url, bool);
+      newLocation = url;
+      $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    });
+    //if browser doesn't support CSS transitions
+    if( !transitionsSupported() ) {
+      loadNewContent(url, bool);
+      newLocation = url;
+    }
+	}
+
+	function loadNewContent(url, bool) {
+		url = ('' == url) ? 'index.html' : url;
+  	var newSection = 'cd-'+url.replace('.html', '');
+  	var section = $('<div class="cd-main-content '+newSection+'"></div>');
+  		
+  	section.load(url+' .cd-main-content > *', function(event){
+      // load new content and replace <main> content with the new one
+      $('main').html(section);
+      //if browser doesn't support CSS transitions - dont wait for the end of transitions
+      var delay = ( transitionsSupported() ) ? 1200 : 0;
+      setTimeout(function(){
+        //wait for the end of the transition on the loading bar before revealing the new content
+        ( section.hasClass('cd-about') ) ? $('body').addClass('cd-about') : $('body').removeClass('cd-about');
+        $('body').removeClass('page-is-changing');
+        $('.cd-loading-bar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+          isAnimating = false;
+          $('.cd-loading-bar').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+        });
+
+        if( !transitionsSupported() ) isAnimating = false;
+      }, delay);
+      
+      if(url!=window.location && bool){
+        //add the new page to the window.history
+        //if the new page was triggered by a 'popstate' event, don't add it
+        window.history.pushState({path: url},'',url);
+      }
+		});
+  }
+
+  function transitionsSupported() {
+    return $('html').hasClass('csstransitions');
+  }
+});
+
+// Parallax
+$(document).ready(function() {
+    
+  var top;
+  $(window).scroll(function() {
+    top = ($(window).scrollTop() * 0.4) + 'px';
+    $('.diagonal .background').css({
+      'background-position': '0 ' + top
+    });
+     $('.diagonal .background2').css({
+      'background-position': '0 ' + top
+    });
+  });
+
+});
